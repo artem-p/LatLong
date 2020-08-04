80078c39a541bff8d3f57b1c11cc6260651aa144
Artyom Pugachev <artempugachev1@gmail.com>
2020-08-04 17:45:50 +0300

    add blank react component

---
+++ scripts/latlong.js
@@ -0,0 +1,13 @@
+class LatLongRoot extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            'Lat Long'
+        )
+    }
+}
