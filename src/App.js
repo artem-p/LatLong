e79df801dda3789ee3fa00a7c4dd55f486edcb6b
Artyom Pugachev <artempugachev1@gmail.com>
2020-09-01 12:45:32 +0300

    draggable marker

--- src/App.js
+++ src/App.js
@@ -15,24 +15,26 @@ const position = [51.505, -0.09]
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
-    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-    iconUrl: require('leaflet/dist/images/marker-icon.png'),
-    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
 });
 
 function App() {
   return (
     <div className="App">
       <Container fluid>
-            <Map className='map' center={position} zoom={13}>
-              <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-              />
-              <Marker position={position}>
-                <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
-              </Marker>
-            </Map>
+        <Map className='map' center={position} zoom={13}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+          />
+          <Marker
+            draggable={true}
+            position={position}>
+            <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+          </Marker>
+        </Map>
       </Container>
     </div>
   );
